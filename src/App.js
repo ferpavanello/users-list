430f0f2dc4728342d25709fbce3fa079ba25fb8d
Fernando <fernando.pavanello2@gmail.com>
2019-06-15 18:37:54 +0000

    Adicionado main.js para controlar os elementos no container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Menu from "./components/menu/menu";
+import Main from "./main";
 
 function App() {
   return (
     <div>
-      <Menu />
+      <Main />
     </div>
   );
 }
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Menu from "./components/menu/menu";
+
+const Main = () => (
+  <main>
+    <Container fixed>
+      <Menu />
+    </Container>
+  </main>
+);
+
+export default Main;
